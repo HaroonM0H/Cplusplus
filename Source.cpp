d8e073110bb5dff416220cc569d5ae959eec3728
Haroon <imtooawesome0@gmail.com>
2026-09-01 20:33:25 +0100

    added program to calculate hypotenuse

---
+++ Source.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <iostream>
+
+int main() {
+	double a{};
+	double b{};
+
+	std::cout << "Enter the triangle base: ";
+	std::cin >> a;
+	std::cout << "Enter the triangle base: ";
+	std::cin >> b;
+
+	a = pow(a, 2);
+	b = pow(b, 2);
+
+	std::cout << "The length of the hypotenuse is: " << sqrt(a + b) << " YEP.";
+
+}
+
+//simple program to calculate hypotenuse of a triangle
