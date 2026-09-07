056b3fed8dfb6b125cac50c45897ade543f4fe3c
Haroon <imtooawesome0@gmail.com>
2026-09-07 02:55:05 +0100

    fixed logic and syntax

--- creditvalidator.cpp
+++ creditvalidator.cpp
@@ -1,49 +1,38 @@
+// creditvalidator.cpp
 #include "creditvalidator.h"
 #include <iostream>
 
-
 int creditValidator() {
+    std::string cardNumber;
+    std::cout << "Enter your card number: ";
+    std::cin >> cardNumber;
 
-	std::string cardNumber;
-	int result = 0;
+    int result = sumOddDigits(cardNumber) + sumEvenDigits(cardNumber);
 
-	result = sumOddDigits() + sumEvenDigits();
-
-	if (result % 10 == 0) {
-		std::cout << "Your card is valid";
-	} else {
-		std::cout << "Your card is invalid";
-	}
-
-	return 0;
+    if (result % 10 == 0) {
+        std::cout << "Your card is valid";
+    } else {
+        std::cout << "Your card is invalid";
+    }
+    return 0;
 }
 
-
-int getDigit(const int number) {
-
-	return number % 10 + (number / 10 % 10);
+int getDigit(int number) {
+    return number % 10 + (number / 10 % 10);
 }
 
-
-int sumOddDigits(const std::string cardNumber) {
-
-	int sum = 0;
-
-	for (int i = cardNumber.size() - 1; i >= 0; i -= 2) {
-		sum += cardNumber[i] - '0';
-	}
-
-	return sum;
+int sumOddDigits(const std::string& cardNumber) {
+    int sum = 0;
+    for (int i = cardNumber.size() - 1; i >= 0; i -= 2) {
+        sum += cardNumber[i] - '0';
+    }
+    return sum;
 }
 
-
-int sumEvenDigits(const std::string cardNumber) {
-	
-	int sum = 0;
-
-	for (int i = cardNumber.size() - 2; i >= 0; i -= 2) {
-		sum += getDigit((cardNumber[i] - '0') * 2);
-	}
-	
-	return sum;
+int sumEvenDigits(const std::string& cardNumber) {
+    int sum = 0;
+    for (int i = cardNumber.size() - 2; i >= 0; i -= 2) {
+        sum += getDigit((cardNumber[i] - '0') * 2);
+    }
+    return sum;
 }
--- creditvalidator.h
+++ creditvalidator.h
@@ -1,6 +1,7 @@
 #pragma once
+#include <string>
 
 int creditValidator();
-int getDigit();
-int sumOddDigits();
-int sumEvenDigits();
+int getDigit(int number);
+int sumOddDigits(const std::string& cardNumber);
+int sumEvenDigits(const std::string& cardNumber);
